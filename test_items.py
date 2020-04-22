ab1e812418f631a7ef28a87fe9e4e41f9984d8c0
vagulyi <v.a.gulyi@gmail.com>
2020-04-22 20:52:57 +0300

    added new files

--- test_items.py
+++ test_items.py
@@ -8,4 +8,4 @@ def test_guest_should_see_cart_button(browser):
     time.sleep(30)
     cart_button = browser.find_element_by_css_selector(".btn-add-to-basket")
     #cart_button.click()
-    assert cart_button, "Should be a basket button"
+    assert cart_button, "Should be a button"
